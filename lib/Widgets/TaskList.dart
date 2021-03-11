11e24bcc0df184f8d3357636b362025048e549eb
Akash J Nair <akashjnair701@gmail.com>
2021-03-11 13:00:24 +0530

    List Builder initialized

--- lib/Widgets/TaskList.dart
+++ lib/Widgets/TaskList.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/Widgets/TaskTile.dart';
+import 'package:todo_app/Model/Task.dart';
 
-class Tasklist extends StatelessWidget {
+class Tasklist extends StatefulWidget {
+  @override
+  _TasklistState createState() => _TasklistState();
+}
+
+class _TasklistState extends State<Tasklist> {
+  List<Task> task = [
+    Task(name: 'Buy MIlk'),
+    Task(name: 'Buy Eggs'),
+    Task(name: 'Buy Bread')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
-      children: [
-        Tasktile('Buy Milk'),
-        Tasktile('Buy Break'),
-        Tasktile('Buy Tea'),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) => Tasktile(
+          listname: task[index].name,
+          ischeked: task[index].isDone,
+          checkBox: (checkBoxSate) {
+            setState(() {
+              task[index].Toggle();
+            });
+          }),
+      itemCount: task.length,
     );
   }
 }
--- lib/Widgets/TaskTile.dart
+++ lib/Widgets/TaskTile.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 class Tasktile extends StatelessWidget {
-  Tasktile(this.listname);
+  Tasktile({this.listname, this.ischeked, this.checkBox});
   final String listname;
+  final bool ischeked;
+  final Function checkBox;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         listname,
         style: TextStyle(
+          decoration: ischeked ? TextDecoration.lineThrough : null,
           fontSize: 20,
         ),
       ),
-      trailing: Checkbox(value: false),
+      trailing: Checkbox(
+        onChanged: checkBox,
+        activeColor: Colors.lightBlueAccent,
+        value: ischeked,
+      ),
     );
   }
 }
